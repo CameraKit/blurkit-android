0558b43c9b6d5abee135ca851de84928a4cf3b7a
Ben Sandee <tbsandee@orangebikelabs.com>
2016-11-29 14:56:26 -0600

    comment out blurkit on main activity for testing -- easier to see flaky animations in fragment

--- demo/src/main/java/com/wonderkiln/blurkit/demo/MainActivity.java
+++ demo/src/main/java/com/wonderkiln/blurkit/demo/MainActivity.java
@@ -1,18 +1,13 @@
 package com.wonderkiln.blurkit.demo;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
-
-import com.wonderkiln.blurkit.BlurLayout;
 
 public class MainActivity extends AppCompatActivity {
 
-    private BlurLayout blurLayout;
+    //private BlurLayout blurLayout;
     private Button showFragmentButton;
     private float movement = 1.6f;
 
@@ -20,18 +15,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        blurLayout = (BlurLayout) findViewById(R.id.blurLayout);
+        //blurLayout = (BlurLayout) findViewById(R.id.blurLayout);
         showFragmentButton = (Button)findViewById(R.id.showFragmentButton);
 
-        final ImageView imageView = (ImageView) findViewById(R.id.imageView);
-        imageView.animate().scaleX(movement).scaleY(movement).setDuration(1500).setListener(new AnimatorListenerAdapter() {
-            @Override
-            public void onAnimationEnd(Animator animation) {
-                super.onAnimationEnd(animation);
-                movement = movement > 1.2f ? 1.2f : 1.6f;
-                imageView.animate().scaleX(movement).scaleY(movement).setDuration(1500).setListener(this).start();
-            }
-        }).start();
+//        final ImageView imageView = (ImageView) findViewById(R.id.imageView);
+//        imageView.animate().scaleX(movement).scaleY(movement).setDuration(1500).setListener(new AnimatorListenerAdapter() {
+//            @Override
+//            public void onAnimationEnd(Animator animation) {
+//                super.onAnimationEnd(animation);
+//                movement = movement > 1.2f ? 1.2f : 1.6f;
+//                imageView.animate().scaleX(movement).scaleY(movement).setDuration(1500).setListener(this).start();
+//            }
+//        }).start();
 
         showFragmentButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -49,12 +44,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        blurLayout.startBlur();
+        //blurLayout.startBlur();
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        blurLayout.pauseBlur();
+        //blurLayout.pauseBlur();
     }
 }
